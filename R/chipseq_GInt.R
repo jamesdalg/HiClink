b2ed583eb7e4d437ec4c88018252301a9cde739a
Dalgleish <dalgleishjl@nih.gov>
2017-11-14 17:39:36 -0500

    added templates for chipseq and annotation functions.

---
+++ R/chipseq_GInt.R
@@ -0,0 +1,19 @@
+#reference: https://github.com/kbroman/pkg_primer/blob/gh-pages/example/stage3/R/plot_crayons.R
+#' A series of methods integrating ChIPseq data and HiC interactions.
+#' 
+#' @param (parameter) (description of function parameter)
+#' 
+#' @param (parameter) (description of function parameter)
+#' 
+#' @return (return value) (description)
+#' @author James L. T. Dalgleish
+#' @seealso \code{\link[arelatedfunction]}
+#' @keywords chipseq HiC HiChip GInteractions
+#' 
+#' @examples 
+#' afunction()
+#' 
+#' @export
+#' @importFrom (needed package) needed functions
+#' 
+#' 
