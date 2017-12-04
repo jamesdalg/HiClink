f8aa4cec0814de19c72b4182bdd62568c37a592b
Dalgleish <dalgleishjl@nih.gov>
2017-12-04 10:50:57 -0500

    additional code for wubrowse functionality

--- R/convertToWubrowse.R
+++ R/convertToWubrowse.R
@@ -12,9 +12,9 @@ hicgenint_full_sig<-hicgenint_full[hicgenint_full$p.value<0.05,]
 #
 #-log(hicgenint_full_sig$p.value)
 #
-hicgenint_full_sig$
+#hicgenint_full_sig$
   
-  convertToWubrowse<-function(hiccontactsdataframe=NULL,pvaluecutoff=0.05,format="HiCcompare",score=foldchange)
+  convertToWubrowse<-function(hiccontactsdataframe=NULL,pvaluecutoff=0.05,format="HiCcompare",score=foldchange,trackformat=T)
   {
     if(format=="HiCcompare")
     {
@@ -22,14 +22,36 @@ hicgenint_full_sig$
       i<-1
       outputdf<-foreach(i=1:nrow(hiccontactsdataframe_sig),.combine="rbind") %do%
       {
+        if(trackformat==F)
+          {
         outputrow<-cbind(
           paste(c(hiccontactsdataframe_sig$chr1[i],hiccontactsdataframe_sig$start1[i],hiccontactsdataframe_sig$end1[i]),collapse=","),
           paste(c(hiccontactsdataframe_sig$chr2[i],hiccontactsdataframe_sig$start2[i],hiccontactsdataframe_sig$end2[i]),collapse=","),
           hiccontactsdataframe_sig$fold.change[i])
         outputrow
+        }
+
+        # if(trackformat==T)
+        # {
+        #   # chr1   \t   111   \t   222   \t   chr2:333-444,55   \t   1   \t   .
+        #   # chr2   \t   333   \t   444   \t   chr1:111-222,55   \t   2   \t   .
+        #   
+        #   outputrowpair<-rbind(c(hiccontactsdataframe_sig$chr1[i],hiccontactsdataframe_sig$start1[i],hiccontactsdataframe_sig$end1[i],
+        #                paste0(hiccontactsdataframe_sig$chr2[i],":",hiccontactsdataframe_sig$start2[i],"-",
+        #                       hiccontactsdataframe_sig$end2[i],",",hiccontactsdataframe_sig$fold.change[i])
+        #                ,i,"."),
+        #                c(hiccontactsdataframe_sig$chr2[i],hiccontactsdataframe_sig$start2[i],hiccontactsdataframe_sig$end2[i],
+        #                  paste0(hiccontactsdataframe_sig$chr1[i],":",hiccontactsdataframe_sig$start1[i],"-",
+        #                         hiccontactsdataframe_sig$end1[i],",",hiccontactsdataframe_sig$fold.change[i])
+        #                  ,nrow(hiccontactsdataframe_sig)+i,"."))
+        # outputrowpair
+        #   }
       }
     }
   }
 test_wubrowse_output<-convertToWubrowse(hiccontactsdataframe = hicgenint_full)
 #library(data.table)
+setwd("W:/dalgleishjl/HiClink/HiClink/")
 write.table(test_wubrowse_output,paste0(getwd(),"/output/test_wubrowse_output.tsv"),sep="\t",quote=F,row.names=F,col.names = F)
+nontrackformat_output<-convertToWubrowse(hiccontactsdataframe = hicgenint_full,trackformat = F)
+write.table(outputdf,"test_wubrowse_nontrack.txt",sep="\t",quote = F,row.names = F,col.names = F)
